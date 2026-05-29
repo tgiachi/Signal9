e1c8853ad8a721f9e1df95f61d5108fba583d24d
Tom <squid@stormwind.it>
2026-05-29 16:39:29 +0200

    feat(persistence): add ScheduleBlockRuleType and ScheduledEntryKind enums

---
+++ src/SignalNine.Persistence/Types/ScheduleBlockRuleType.cs
@@ -0,0 +1,8 @@
+namespace SignalNine.Persistence.Types;
+
+public enum ScheduleBlockRuleType
+{
+    Pin = 0,
+    Series = 1,
+    TagPool = 2
+}
---
+++ src/SignalNine.Persistence/Types/ScheduledEntryKind.cs
@@ -0,0 +1,8 @@
+namespace SignalNine.Persistence.Types;
+
+public enum ScheduledEntryKind
+{
+    Media = 0,
+    Bumper = 1,
+    Commercial = 2
+}
